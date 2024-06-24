dd12c333e3241e5e7e276efc702ce436a7d1be09
Jaden <jaden.franks@gmail.com>
2024-06-24 17:48:35 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(array){
+  const wins = array.find(recodObject => recodObject.result === "W")
+  if(wins === undefined){
+    return undefined
+  }
+  return wins.year
+}
+  
+
+
